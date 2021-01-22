17d9959ec959bba79289cfc1d17e13f4a3fbd835
Ava <ava.soyeon.kim@gmail.com>
2021-01-22 22:56:04 +0900

    add filter feature

--- src/app.js
+++ src/app.js
@@ -39,10 +39,82 @@ function createElement(item) {
   return div;
 }
 
+function onRadioClick(event, items) {
+  const key = event.target.name;
+  const value = event.target.value;
+  if (key == null || value == null) {
+    return;
+  }
+  checkOhterFilter(items, key, value);
+}
+
+function checkOhterFilter(items, key, value) {
+  const enroll = document.querySelector('input[name=enrolled]:checked');
+  const classGroup = document.querySelector('input[name=class]:checked');
+
+  if (enroll == null || classGroup == null) {
+    items.forEach((item) => {
+      if (item.dataset[key] === value) {
+        item.classList.remove('invisible');
+      } else {
+        item.classList.add('invisible');
+      }
+    });
+    return;
+  }
+
+  let secondKey = '';
+  let secondValue = '';
+  if (key == 'class') {
+    secondKey = 'enrolled';
+    secondValue = `${enroll.value}`;
+  } else {
+    secondKey = 'class';
+    secondValue = `${classGroup.value}`;
+  }
+
+  updateCards(items, key, value, secondKey, secondValue);
+}
+
+function updateCards(items, key, value, secondKey, secondValue) {
+  items.forEach((item) => {
+    if (
+      item.dataset[key] === value &&
+      item.dataset[secondKey] === secondValue
+    ) {
+      item.classList.remove('invisible');
+    } else {
+      item.classList.add('invisible');
+    }
+  });
+}
+
+function resetFilter(items) {
+  items.forEach((item) => {
+    item.classList.remove('invisible');
+  });
+}
+
 loadCards().then((items) => {
   const elements = items.map(createElement);
   const container = document.querySelector('.cards');
   container.append(...elements);
+
+  const classFilter = document.querySelector('.filter-group__class');
+  const enrollFilter = document.querySelector('.filter-group__enroll');
+  const resetBtn = document.querySelector('.reset-btn');
+
+  classFilter.addEventListener('click', (event) => {
+    onRadioClick(event, elements);
+  });
+
+  enrollFilter.addEventListener('click', (event) => {
+    onRadioClick(event, elements);
+  });
+
+  resetBtn.addEventListener('click', () => {
+    resetFilter(elements);
+  });
 });
 
 const filterOpenBtn = document.querySelector('.tab-menu__btn');
